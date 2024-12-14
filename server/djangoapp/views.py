1cf52d1ceb32851d372cc38bb32c26773b4f7e36
“sinensis1511” <saramartignoni@yahoo.it>
2024-12-14 08:35:43 -0500

    lint 3

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -31,7 +31,8 @@ def login_user(request):
     if user is not None:
         # If user is valid, call login method to login current user
         login(request, user)
-        data = {"userName": username, "status": "Authenticated"}
+        data = {"userName": username,
+        "status": "Authenticated"}
     return JsonResponse(data)
 
 
@@ -79,10 +80,12 @@ def registration(request):
         )
         # Login the user and redirect to list page
         login(request, user)
-        data = {"userName": username, "status": "Authenticated"}
+        data = {"userName": username,
+        "status": "Authenticated"}
         return JsonResponse(data)
     else:
-        data = {"userName": username, "error": "Already Registered"}
+        data = {"userName": username, "error":
+        "Already Registered"}
         return JsonResponse(data)
 
 
@@ -93,7 +96,8 @@ def get_dealerships(request, state="All"):
     else:
         endpoint = "/fetchDealers/" + state
     dealerships = get_request(endpoint)
-    return JsonResponse({"status": 200, "dealers": dealerships})
+    return JsonResponse({"status": 200,
+    "dealers": dealerships})
 
 
 def get_dealers(request):
@@ -102,12 +106,15 @@ def get_dealers(request):
         dealers = get_request("/fetchDealers")
         # Verifica se la risposta è corretta
         if "status" in dealers and dealers["status"] == 200:
-            return JsonResponse({"status": 200, "dealers": dealers})
+            return JsonResponse({"status": 200,
+            "dealers": dealers})
         else:
-            return JsonResponse({"status": 400, "message": "Unable to fetch dealers"})
+            return JsonResponse({"status": 400,
+            "message": "Unable to fetch dealers"})
     except Exception as e:
         logger.error(f"Error fetching dealers: {e}")
-        return JsonResponse({"status": 500, "message": "Internal Server Error"})
+        return JsonResponse({"status": 500,
+        "message": "Internal Server Error"})
 
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
@@ -127,9 +134,11 @@ def get_dealer_reviews(request, dealer_id):
                 sentiment  # Aggiungi il sentimento al dizionario della recensione
             )
 
-        return JsonResponse({"status": 200, "reviews": reviews})
+        return JsonResponse({"status": 200,
+        "reviews": reviews})
     else:
-        return JsonResponse({"status": 400, "message": "Bad Request"})
+        return JsonResponse({"status": 400,
+        "message": "Bad Request"})
 
 
 # Create a `get_dealer_details` view to render the dealer details
@@ -139,9 +148,11 @@ def get_dealer_details(request, dealer_id):
     if dealer_id:
         endpoint = "/fetchDealer/" + str(dealer_id)
         dealership = get_request(endpoint)
-        return JsonResponse({"status": 200, "dealer": dealership})
+        return JsonResponse({"status": 200,
+        "dealer": dealership})
     else:
-        return JsonResponse({"status": 400, "message": "Bad Request"})
+        return JsonResponse({"status": 400,
+        "message": "Bad Request"})
 
 
 # Create a `add_review` view to submit a review
@@ -151,13 +162,15 @@ def add_review(request):
     if request.user.is_anonymous == False:
         data = json.loads(request.body)
         try:
-            response = post_review(data)
+            #response = post_review(data)
             return JsonResponse({"status": 200})
         except Exception as e:
             print(f"Error: {e}")
-            return JsonResponse({"status": 401, "message": "Error in posting review"})
+            return JsonResponse({"status": 401,
+            "message": "Error in posting review"})
     else:
-        return JsonResponse({"status": 403, "message": "Unauthorized"})
+        return JsonResponse({"status": 403,
+        "message": "Unauthorized"})
 
 
 def get_cars(request):
@@ -169,7 +182,8 @@ def get_cars(request):
 
     car_models = CarModel.objects.select_related("car_make")
     cars = [
-        {"CarModel": car_model.name, "CarMake": car_model.car_make.name}
+        {"CarModel": car_model.name,
+        "CarMake": car_model.car_make.name}
         for car_model in car_models
     ]
 
