b76fbd0bf8d79f488b3629699197627eb8c901b4
“sinensis1511” <saramartignoni@yahoo.it>
2024-12-14 08:05:04 -0500

    resolve linting problems

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,19 +1,16 @@
-# from django.contrib import admin
-# from .models import related models
-
-
-# Register your models here.
-
-# CarModelInline class
-
-# CarModelAdmin class
-
-# CarMakeAdmin class with CarModelInline
-
-# Register models here
 from django.contrib import admin
 from .models import CarMake, CarModel
 
-# Registering models with their respective admins
-admin.site.register(CarMake)
+# CarModelInline class
+class CarModelInline(admin.ModelAdmin):
+    fields = ('name', 'description')
+# CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    fields = ('name')
+# CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
+
+# Register your models here.
 admin.site.register(CarModel)
+admin.site.register(CarMake)
